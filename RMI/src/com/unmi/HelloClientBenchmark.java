f129f264177a4d55355830ef5f5047cd1a033caf
pugwoo <pugwoo@gmail.com>
2012-11-06 14:20:14 +0800

    rmi测试

---
+++ RMI/src/com/unmi/HelloClientBenchmark.java
@@ -0,0 +1,36 @@
+package com.unmi;
+
+import java.rmi.Naming;
+
+/**
+ * @author nickxie
+ * 2012-11-6 下午02:19:37
+ */
+public class HelloClientBenchmark {
+	/**
+	 * 查找远程对象并调用远程方法
+	 */
+	public static void main(String[] argv) {
+		if (argv.length == 0) {
+			System.out.println("usage: java HelloClientBenchmark times");
+			return;
+		}
+
+		int times = Integer.valueOf(argv[0]);
+
+		try {
+			HelloInterface hello = (HelloInterface) Naming.lookup("Hello");
+
+			// 如果要从另一台启动了RMI注册服务的机器上查找hello实例
+			// HelloInterface hello =
+			// (HelloInterface)Naming.lookup("//192.168.1.105:1099/Hello");
+
+			// 调用远程方法
+			for (int i = 0; i < times; i++) {
+				hello.say();
+			}
+		} catch (Exception e) {
+			System.out.println("HelloClient exception: " + e);
+		}
+	}
+}
